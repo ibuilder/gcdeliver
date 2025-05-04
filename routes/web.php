1fef03e4ed84ffced363694bcbca270673e1a986
iphoenix <iphoenix@gmail.com>
2025-05-04 23:09:19 +0000

    routes-fix

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::resource('users', UserController::class)->except(['destroy']);
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::resource('users', UserController::class)->except(['show','edit','update']);
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::resource('partners', PartnerController::class)->except(['store', 'index', 'show', 'create', 'edit', 'update']);
 Route::post('/partners', [PartnerController::class, 'store'])->name('partners.store');
 Route::get('/partners', [PartnerController::class, 'index'])->name('partners.index');
@@ -33,10 +31,7 @@ Route::get('/partners/create', [PartnerController::class, 'create'])->name('part
 Route::get('/partners/{id}/edit',[PartnerController::class, 'edit'])->name('partners.edit');
 Route::put('/partners/{id}', [PartnerController::class, 'update'])->name('partners.update');
 
-Route::resource('projects', ProjectController::class)->except(['store', 'index', 'show', 'create', 'edit', 'update']);
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('projects.show');
+Route::resource('projects', ProjectController::class)->except(['create','store', 'index', 'show', 'edit', 'update']);
 Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
 Route::get('/projects/{id}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
 Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('projects.update');
@@ -52,13 +47,8 @@ Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update
 Route::resource('manpower_entries', ManpowerEntryController::class);
 Route::get('/manpower_entries/create', [ManpowerEntryController::class, 'create'])->name('manpower_entries.create');
 Route::get('/manpower_entries', [ManpowerEntryController::class, 'index'])->name('manpower_entries.index');
-
-Route::get('/gantt-chart', [GanttChartController::class, 'generateGanttChart'])->name('gantt_chart');
-Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update');
-
-Route::resource('deliveries', DeliveryController::class)->except(['store', 'index', 'show', 'create', 'edit', 'update']);
+Route::resource('deliveries', DeliveryController::class)->except(['create','store', 'index', 'show', 'edit', 'update']);
 Route::get('/deliveries/create', [DeliveryController::class, 'create'])->name('deliveries.create');
-Route::get('/deliveries/{id}/edit', [DeliveryController::class, 'edit'])->name('deliveries.edit');
 Route::get('/deliveries', [DeliveryController::class, 'index'])->name('deliveries.index');
 Route::get('/deliveries/{id}', [DeliveryController::class, 'show'])->name('deliveries.show');
 Route::put('/deliveries/{id}', [DeliveryController::class, 'update'])->name('deliveries.update');
@@ -79,7 +69,10 @@ Route::get('/daily_reports', [DailyReportController::class, 'index'])->name('dai
 Route::get('/daily_reports/{id}/edit', [DailyReportController::class, 'edit'])->name('daily_reports.edit');
 Route::put('/daily_reports/{id}', [DailyReportController::class, 'update'])->name('daily_reports.update');
 
-Route::resource('item_partners', ItemPartnerController::class)->except(['index']);
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+Route::get('/projects/{id}', [ProjectController::class, 'show'])->name('projects.show');
+Route::resource('item_partners', ItemPartnerController::class)->except(['index',]);
 Route::get('/item_partners', [ItemPartnerController::class, 'index'])->name('item_partners.index');
 
 Route::resource('delivery_items', DeliveryItemController::class)->except(['index']);
@@ -87,3 +80,6 @@ Route::get('/delivery_items', [DeliveryItemController::class, 'index'])->name('d
 
 Route::resource('activity_dependencies', ActivityDependencyController::class)->except(['index']);
 Route::get('/activity_dependencies', [ActivityDependencyController::class, 'index'])->name('activity_dependencies.index');
+Route::get('/gantt-chart', [GanttChartController::class, 'generateGanttChart'])->name('gantt_chart');
+Route::get('/deliveries/{id}/edit', [DeliveryController::class, 'edit'])->name('deliveries.edit');
+Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update');
