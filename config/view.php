7d96be3bf2c3489ab045a4440fad263edd7f6288
iphoenix <iphoenix@gmail.com>
2025-05-04 23:57:44 +0000

    vonfig-view

---
+++ config/view.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specify
+    | an array of paths that should be checked for your view templates.
+    | Of course, Laravel will efficiently cache it to speed up view loading.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where all the compiled Blade templates will be
+    | stored for your application. Typically, this is within the storage
+    | directory. However, as usual, you are free to change this value.
+    |
+    */
+
+    'compiled' => storage_path('framework/views'),
+
+    'relative_hash' => false,
+
+    'cache' => true,
+
+    'compiled_extension' => 'php',
+
+];
